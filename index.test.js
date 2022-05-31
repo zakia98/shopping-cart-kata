240d6696fc99383481bb3383630a2d165548b9fc
Ameen <ameenahmed1998@gmail.com>
2022-05-31 16:51:51 +0100

    Added tests for error handling and inputs.

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 let total = require('./index')
 
-describe('total', () => {
+describe('total test set 1', () => {
     let prices = {
         A:0.50,
         B:0.75,
@@ -77,6 +77,85 @@ describe('total', () => {
 })
 
 
+describe('total test set error handling',  () => {
+    test(`A cart item which has no price`, () => {
+        let prices = {
+            A:0.50,
+            B:0.75,
+        };
+        let discounts = {
+            B:{
+                qualifier: 2,
+                discount: 0.25
+            },
+            C: {
+                qualifier: 4,
+                discount: 0.25
+            }
+        };
+        let cart = ['C', 'B', 'B']
+        expect(() => total(cart, prices, discounts)).toThrow('The price of C is not defined!')
+    });
 
+    test(`A cart item's price is not an number`, () => {
+        let prices = {
+            A:0.50,
+            B:"0.75",
+            C:0.25
+        };
+        let discounts = {
+            B:{
+                qualifier: 2,
+                discount: 0.25
+            },
+            C: {
+                qualifier: 4,
+                discount: 0.25
+            }
+        };
+        let cart = ['C', 'B', 'B'];
+        expect(() => total(cart, prices, discounts)).toThrow('The price of B is not a number')
+    });
+
+    test(`Discount qualifier is not a number`, () => {
+        let prices = {
+            A:0.50,
+            B:0.75,
+            C:0.25
+        };
+        let discounts = {
+            B:{
+                qualifier: "2a",
+                discount: 0.50
+            },
+            C: {
+                qualifier: 4,
+                discount: 0.25
+            }
+        };
+        let cart = ['C', 'B', 'B'];
+        expect(() => total(cart, prices, discounts)).toThrow('The discount qualifier for B is not a number')
+    });
+    
+    test(`Qualifier is negative`, () => {
+        let prices = {
+            A:0.50,
+            B:0.75,
+            C:0.25
+        };
+        let discounts = {
+            B:{
+                qualifier: -1,
+                discount: 0.50
+            },
+            C: {
+                qualifier: 4,
+                discount: 0.25
+            }
+        };
+        let cart = ['C', 'B', 'B'];
+        expect(() => total(cart, prices, discounts)).toThrow('The discount qualifier for B is less than 1!')
+    })
+})
 
 
