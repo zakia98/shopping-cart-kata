35224ebaeedeb79b3fcfedf9a9e4feb03d060440
Ameen <ameenahmed1998@gmail.com>
2022-05-31 16:06:02 +0100

    Added describe block which provides global prices for tests

--- index.test.js
+++ index.test.js
@@ -1,71 +1,82 @@
 let total = require('./index')
 
-test('No discount, adds items of same type C', () => {
-    let cart = ['C', 'C', 'C'];
-    let discounts = {}
-    let totalPrice = total(cart, discounts)
+describe('total', () => {
+    let prices = {
+        A:0.50,
+        B:0.75,
+        C:0.25
+    };
+
+    test('No discount, adds items of same type C', () => {
+        let cart = ['C', 'C', 'C'];
+        let discounts = {}
+        let totalPrice = total(cart, prices, discounts)
+        
+        expect(totalPrice).toEqual(0.75)
+    });
+
+    test('No discount, adds items of different types', () => {
+        let cart = ['A', 'A', 'C'];
+        let discounts = {}
+        let totalPrice = total(cart, prices, discounts)
+        
+        expect(totalPrice).toEqual(1.25)
+    });
+
+    test('Discount on 3 items of same type B', () => {
+        let cart = ['B', 'B', 'B'];
+        let discounts = {
+            B:{
+                qualifier: 2,
+                discount: 0.25
+            },
+            C: {
+                qualifier: 4,
+                discount: 0.25
+            }
+        }
+        let totalPrice = total(cart, prices, discounts);
+
+        expect(totalPrice).toEqual(2.00);
+    });
+
+    test('Buy 3, get 1 free', () => {
+        let cart = ['C', 'C', 'C', 'C'];
+        let discounts = {
+            B:{
+                qualifier: 2,
+                discount: 0.25
+            },
+            C: {
+                qualifier: 4,
+                discount: 0.25
+            }
+        }
+        let totalPrice = total(cart, prices, discounts);
+
+        expect(totalPrice).toEqual(0.75);
+    });
+
+    test('Two discounts applied in one transaction', () => {
+        let cart = ['B', 'B', 'C', 'C', 'C', 'C'];
+        let discounts = {
+            B:{
+                qualifier: 2,
+                discount: 0.25
+            },
+            C: {
+                qualifier: 4,
+                discount: 0.25
+            }
+        }
+        let totalPrice = total(cart, prices, discounts);
+
+        expect(totalPrice).toEqual(2.00);
+    });
     
-    expect(totalPrice).toEqual(0.75)
-});
+})
 
-test('No discount, adds items of different types', () => {
-    let cart = ['A', 'A', 'C'];
-    let discounts = {}
-    let totalPrice = total(cart, discounts)
-    
-    expect(totalPrice).toEqual(1.25)
-});
 
-test('Discount on 3 items of same type B', () => {
-    let cart = ['B', 'B', 'B'];
-    let discounts = {
-        B:{
-            qualifier: 2,
-            discount: 0.25
-        },
-        C: {
-            qualifier: 4,
-            discount: 0.25
-        }
-    }
-    let totalPrice = total(cart, discounts);
-
-    expect(totalPrice).toEqual(2.00);
-});
-
-test('Buy 3, get 1 free', () => {
-    let cart = ['C', 'C', 'C', 'C'];
-    let discounts = {
-        B:{
-            qualifier: 2,
-            discount: 0.25
-        },
-        C: {
-            qualifier: 4,
-            discount: 0.25
-        }
-    }
-    let totalPrice = total(cart, discounts);
-
-    expect(totalPrice).toEqual(0.75);
-});
-
-test('Two discounts applied in one transaction', () => {
-    let cart = ['B', 'B', 'C', 'C', 'C', 'C'];
-    let discounts = {
-        B:{
-            qualifier: 2,
-            discount: 0.25
-        },
-        C: {
-            qualifier: 4,
-            discount: 0.25
-        }
-    }
-    let totalPrice = total(cart, discounts);
-
-    expect(totalPrice).toEqual(2.00);
-});
 
 
 
