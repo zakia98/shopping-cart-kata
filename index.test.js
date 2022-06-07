25877c7f53a69fcb72a913d1c3cc0fc0eee12df3
Ameen <ameenahmed1998@gmail.com>
2022-06-07 11:37:54 +0100

    Rewrote test descriptions to follow Given When Then structure.

--- index.test.js
+++ index.test.js
@@ -2,83 +2,69 @@ let shoppingCart = require('./index');
 let total = shoppingCart.total
 let calculateDiscounts = shoppingCart.calculateDiscounts
 
-describe('total test set 1', () => {
+describe('Given no discounts', () => {
     let prices = {
         A:0.50,
         B:0.75,
         C:0.25
     };
+    let discounts = {}
 
-    test('No discount, adds items of same type C', () => {
+    test('Total three identical items of type C', () => {
         let cart = ['C', 'C', 'C'];
-        let discounts = {}
         let totalPrice = total(cart, prices, discounts)
         
         expect(totalPrice).toEqual(0.75)
     });
 
-    test('No discount, adds items of different types', () => {
+    test('Total three items of different types', () => {
         let cart = ['A', 'A', 'C'];
-        let discounts = {}
         let totalPrice = total(cart, prices, discounts)
         
         expect(totalPrice).toEqual(1.25)
     });
+})
 
-    test('Discount on 3 items of same type B', () => {
-        let cart = ['B', 'B', 'B'];
-        let discounts = {
-            B:{
-                qualifier: 2,
-                discount: 0.25
-            },
-            C: {
-                qualifier: 4,
-                discount: 0.25
-            }
+describe('Given two discount types, "multi-buy" and "buy n get y free" exist', () => {
+    let prices = {
+        A:0.50,
+        B:0.75,
+        C:0.25
+    };
+    let discounts = {
+        B:{
+            qualifier: 2,
+            discount: 0.25
+        },
+        C: {
+            qualifier: 4,
+            discount: 0.25
         }
+    }
+
+    test('Multi-buy on 3 items of type B', () => {
+        let cart = ['B', 'B', 'B'];
         let totalPrice = total(cart, prices, discounts);
 
         expect(totalPrice).toEqual(2.00);
     });
 
-    test('Buy 3, get 1 free', () => {
+    test('Buy 4 receive 1 free on items of type C', () => {
         let cart = ['C', 'C', 'C', 'C'];
-        let discounts = {
-            B:{
-                qualifier: 2,
-                discount: 0.25
-            },
-            C: {
-                qualifier: 4,
-                discount: 0.25
-            }
-        }
         let totalPrice = total(cart, prices, discounts);
 
         expect(totalPrice).toEqual(0.75);
     });
 
-    test('Two discounts applied in one transaction', () => {
+    test('two discounts can be applied in one transaction', () => {
         let cart = ['B', 'B', 'C', 'C', 'C', 'C'];
-        let discounts = {
-            B:{
-                qualifier: 2,
-                discount: 0.25
-            },
-            C: {
-                qualifier: 4,
-                discount: 0.25
-            }
-        }
         let totalPrice = total(cart, prices, discounts);
 
         expect(totalPrice).toEqual(2.00);
-    });
-    
-});
+    })
+})
 
-describe('calculateDiscounts', () => {
+describe('Given the following discount values', () => {
     let discounts = {
         B:{
             qualifier: 2,
@@ -102,7 +88,7 @@ describe('calculateDiscounts', () => {
         expect(discount).toEqual(0.25)
     });
 
-    test('Multi-buy and Get 1 item free disconts', () => {
+    test('Multi-buy and Get 1 item free discounts in same transaction', () => {
         cart = ['B', 'B', 'C', 'C', 'C', 'C'];
         let discount = calculateDiscounts(cart, discounts)
         expect(discount).toEqual(0.50)
@@ -110,7 +96,7 @@ describe('calculateDiscounts', () => {
 })
 
 
-describe('total test set error handling',  () => {
+describe('Given errors in input data',  () => {
     test(`A cart item which has no price`, () => {
         let prices = {
             A:0.50,
